764bf113b3b08a49328a25e528a8866c0b8fdc2d
JuliaKaz <106685861+JuliaKaz@users.noreply.github.com>
2022-10-24 19:24:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlalchemy
 
-db = "postgresql://postgres:VJK-1824-skn@localhost:5432/music"
+db = "postgresql://postgres:пароль@localhost:5432/music"
 engine = sqlalchemy.create_engine(db)
 connection = engine.connect()
 
